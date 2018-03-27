8aeead377ba65762f2b00c31b0d6c63ee351fce7
Michael <michaelaraujo963@gmail.com>
2018-03-26 21:30:43 -0300

    Versão 2.0

---
+++ src/app/heroes/domain/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from "./hero";
+
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Flash Reverso'},
+  {id: 12, name: 'Cisco Ramon'},
+  {id: 13, name: 'Zoom'},
+  {id: 14, name: 'Nevasca'},
+  {id: 15, name: 'Savitar'},
+  {id: 16, name: 'Devoe'},
+  {id: 17, name: 'Kid Flash'},
+]
